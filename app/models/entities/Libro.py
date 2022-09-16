e229f7d999b9eab619d17e5df2a3b40059a355cc
Anthonne <luangapa@gmail.com>
2022-09-16 11:03:29 -0500

    4.6.1 Creacion entidades

---
+++ app/models/entities/Libro.py
@@ -0,0 +1,7 @@
+class Libr():
+    def __init__(self, isbn, titulo, autor, anoedicion, precio):
+        self.isbn = isbn
+        self.titulo = titulo
+        self.autor = autor
+        self.anoedicion = anoedicion
+        self.precio = precio
