e3e77309a706f9783302887d21eef70a1326fc7f
Anthonne <luangapa@gmail.com>
2022-09-16 15:25:45 -0500

    5.1 done gestion y encriptado de contraseñas

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,6 @@ db = MySQL(app)
 def index():
     return render_template('index.html')
 
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     """
--- app/models/entities/Usuario.py
+++ app/models/entities/Usuario.py
@@ -1,7 +1,17 @@
-class usuario:
-    
+from distutils.util import check_environ
+from lib2to3.pgen2.pgen import generate_grammar
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Usuario():
+
     def __init__(self, id, usuario, password, tipousuario):
         self.id = id
-        self.usuario = usuario
+        self.Usuario = usuario
         self.password = password
         self.tipousuario = tipousuario
+
+    def encriptar_password(password):
+        encriptado = generate_password_hash(password)
+        coincide = check_password_hash(encriptado, password)
+        return coincide
